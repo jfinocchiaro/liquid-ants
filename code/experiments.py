23e6ed911038c644ada8068d9254f3eee1f4659b
Gabriel P. Andrade <gpandrad@umass.edu>
2019-05-09 20:14:23 -0600

    More figs and data

--- code/experiments.py
+++ code/experiments.py
@@ -2,13 +2,13 @@ import driver as dr
 import numpy as np
 
 def main():
-    m_max = 50
+    m_max = 25
     obj_size = 1
     default_radius = 5
     default_num_agents = 20
     default_vol_prob = 0.01
-    TIME_LIMIT = 3000
-    num_exp = 5
+    TIME_LIMIT = 1500
+    num_exp = 40
 
     ms = np.linspace(10, m_max ,m_max+1-10, endpoint=True, dtype=int)
     radii = np.linspace(1,m_max,m_max+1, endpoint=True, dtype=int)
@@ -18,21 +18,39 @@ def main():
 
     m=m_max
 
-    time_results = np.zeros((radii.size, num_agents.size, num_exp))
-    for i in range(radii.size):
-        for j in range(num_agents.size):
-            for k in range(num_exp):
-                t = dr.main(m, num_agents[j], 0, radii[i], 1, 5000, default_vol_prob, TIME_LIMIT = TIME_LIMIT)
-                time_results[i,j,k] = t
-                print 'Time ' + str(t) + ' radius ' + str(radii[i]) + ' num_agents ' + str(num_agents[j])
+    # time_results = np.zeros((radii.size, num_agents.size, num_exp))
+    # for i in range(radii.size):
+    #     for j in range(num_agents.size):
+    #         for k in range(num_exp):
+    #             t = dr.main(m, num_agents[j], 0, radii[i], 1, 5000, default_vol_prob, TIME_LIMIT = TIME_LIMIT)
+    #             time_results[i,j,k] = t
+    #             print 'Time ' + str(t) + ' radius ' + str(radii[i]) + ' num_agents ' + str(num_agents[j])
+    # for i in range(num_exp):
+    #     print time_results[:,:,i]
+
+    # print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    # print np.mean(time_results, axis = 2)
+
+    # filename = 'objsize' + str(obj_size) + '_numexp' + str(num_exp) + '_timelimit' + str(TIME_LIMIT) + '_m' + str(m)
+    # np.save(filename, time_results)
+
+    probability_results = np.zeros([probabilities.size,num_exp])
+    for i in range(probabilities.size):
+        for k in range(num_exp):
+            t = dr.main(m, m, 0, 4, 1, 5000, probabilities[i], TIME_LIMIT = TIME_LIMIT)
+            probability_results[i,k] = t
+            print 'Time ' + str(t) + ' prob ' + str(probabilities[i])
     for i in range(num_exp):
-        print time_results[:,:,i]
+        print probability_results[:,i]
 
     print '~~~~~~~~~~~~~~~~~~~~~~~~~~'
-    print np.mean(time_results, axis = 2)
+    print np.mean(probability_results, axis = 1)
 
-    filename = 'objsize' + str(obj_size) + '_numexp' + str(num_exp) + '_timelimit' + str(TIME_LIMIT) + '_m' + str(m)
-    np.save(filename, time_results)
+    filename = 'probability_results_objsize' + str(obj_size) + '_numexp' + str(num_exp) + '_timelimit' + str(TIME_LIMIT) + '_m' + str(m) + '_r' + str(4)
+    np.save(filename, probability_results)
+
+
+
 
 
 if __name__ == '__main__':
--- code/plot_data.py
+++ code/plot_data.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     # to_plot = np.load('../data/no_huddle/num_exp_10/objsize1_numexp10_timelimit1500_m25.npy')
     to_plot = np.load('../data/no_huddle/num_exp_25/objsize1_numexp25_timelimit3000_m25.npy')
+    to_plot_too = np.load('../data/no_huddle/num_exp_100/objsize1_numexp100_timelimit1500_m25.npy')
+    to_plot_too2 = np.load('../data/no_huddle/num_exp_75/objsize1_numexp75_timelimit1500_m25_2.npy')
 
     # print(to_plot.shape)
     # print(to_plot_too.shape)
@@ -11,9 +13,10 @@ if __name__ == "__main__":
 
 
     np.clip(to_plot,0,1501,out=to_plot)
-    # to_plot = np.concatenate((to_plot, to_plot_too), axis=2)
+    to_plot = np.concatenate((to_plot, to_plot_too), axis=2)
+    to_plot = np.concatenate((to_plot, to_plot_too2), axis=2)
 
-    # print(to_plot.shape)
+    print(to_plot.shape)
 
     mean = np.mean(to_plot, axis = 2)
     # varience = np.var(to_plot, axis = 2)
@@ -25,7 +28,7 @@ if __name__ == "__main__":
     mappable = ax.imshow(mean, origin='lower', cmap = 'jet')
     ax.xaxis.set_ticks_position('bottom')
 
-    ax.set_title('Average of Runtime over $25$ simulations on $25$ by $25$ lattice', fontsize=22)
+    ax.set_title('Average of Runtime over $200$ simulations on $25$ by $25$ lattice', fontsize=22)
     ax.set_xlabel('Number of agents', fontsize=18)
     ax.set_ylabel('r', fontsize=18)
 
@@ -35,3 +38,20 @@ if __name__ == "__main__":
     fig.colorbar(mappable)
 
     plt.show()
+
+
+    # to_plot = np.load('../data/no_huddle/probability_of_volunteering/probability_results_objsize1_numexp10_timelimit1500_m25_r4.npy')
+    # # to_plot_too = np.load('../data/no_huddle/probability_of_volunteering/probability_results_objsize1_numexp40_timelimit1500_m25.npy')
+    # probabilities = np.linspace(0,1,20, endpoint=False)
+
+    # # to_plot = np.concatenate((to_plot, to_plot_too), axis=1)
+
+    # mean = np.mean(to_plot, axis = 1)
+    # varience = np.var(to_plot, axis = 1)
+    # std = np.std(to_plot, axis = 1)
+
+    # plt.plot(probabilities, mean, 'x-')
+    # # plt.plot(probabilities, varience, 'o-')
+    # plt.plot(probabilities, std, '.--')
+
+    # plt.show()
